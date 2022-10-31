b0940d2717e2ba7d27d36711f449a36634835954
maddy135 <www.ghostrider125@gmail.com>
2022-10-31 09:45:43 +0530

    front end completed

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+var _ = require('lodash');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static('public'));
+app.get('/',(req,res)=>{
+    res.send(__dirname+"public/index.html")
+
+})
+
+
+app.listen(3000 , ()=>{
+    console.log("server running in 3000");
+})
