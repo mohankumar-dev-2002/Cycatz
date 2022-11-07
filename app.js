94290ea52bde65d54e4caca5c9dffb72f7f3393c
maddy135 <www.ghostrider125@gmail.com>
2022-11-07 12:18:55 +0530

    delete admin login

--- app.js
+++ app.js
@@ -11,12 +11,16 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.send(__dirname + "public/index.html")
 
-})
+});
 
 app.post('/', (req, res) => {
     const name = req.body.name;
     console.log(name);
-})
+    console.log(req)
+});
+
+
+
 app.listen(3000, () => {
     console.log("server running in 3000");
 })
